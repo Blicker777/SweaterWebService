6163e6c5890d21baa3d74e64e33c1a25647c25a7
Blicker777 <GitHub7>
2020-01-25 15:35:55 +0300

    Добавил связь между таблицами Message и User

---
+++ src/main/java/com/example/sweater/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.sweater.service;
+
+import com.example.sweater.domain.User;
+import com.example.sweater.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        return userRepo.findByUsername(userName);
+    }
+}
